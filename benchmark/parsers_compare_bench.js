88e6fa37241b8633924e3670eb5c44d118562be7
Soel <shachar.soel@sap.com>
2018-06-29 18:32:15 +0300

    Chevrotain Parser:
    - Minor performance improvements using Caching to avoid reinitializing arrays.
    - Use string literals for simple Token patterns.
    - Enable flag to ensure lexer optimizations.

--- benchmark/parsers_compare_bench.js
+++ benchmark/parsers_compare_bench.js
@@ -7,42 +7,45 @@ const chevParse = require("../lib/less/parser/chev_css_parser").parseCss;
 const less = require("../lib/less-node");
 
 const sample = fs
-  .readFileSync(path.join(__dirname, "./samples/10k.css"), "utf8")
-  .toString();
+	.readFileSync(path.join(__dirname, "./samples/10k.css"), "utf8")
+	.toString();
 
 function newSuite(name) {
-  return new Benchmark.Suite(name, {
-    onStart: () => console.log(`\n\n${name}`),
-    onCycle: event => console.log(String(event.target)),
-    onComplete: function() {
-      console.log("Fastest is " + this.filter("fastest").map("name"));
-    }
-  });
+	return new Benchmark.Suite(name, {
+		onStart: () => console.log(`\n\n${name}`),
+		onCycle: event => console.log(String(event.target)),
+		onComplete: function () {
+			console.log("Fastest is " + this.filter("fastest").map("name"));
+		}
+	});
 }
 
 function parseChevrotain(text) {
-  const parseResult = chevParse(sample);
-  if (parseResult.lexErrors.length > 0 || parseResult.parseErrors.length > 0) {
-    throw "Oops I did it again";
-  }
+	const parseResult = chevParse(sample);
+	if (parseResult.lexErrors.length > 0 || parseResult.parseErrors.length > 0) {
+		throw "Oops I did it again";
+	}
 }
-newSuite("10k pure CSS input")
-  // The Chevrotain parser does not output any structure yet so the comparison is unfair.
-  // Meaning that Chevrotain has unfair advantage in this benchmark.
-  .add("Chevrotain", () => parseChevrotain(sample))
-  .add("Less Parser", {
-    defer: true,
-    fn: function(deferred) {
-      less.parse(sample, {}, function(err, root, imports, options) {
-        if (err) {
-          less.writeError(err);
-          process.exit(3);
-        }
 
-        deferred.resolve();
-      });
-    }
-  })
-  .run({
-    async: false
-  });
+newSuite("10k pure CSS input")
+// The Chevrotain parser does not output any structure yet so the comparison is unfair.
+// Meaning that Chevrotain has unfair advantage in this benchmark.
+	.add("Less Parser", {
+		defer: true,
+		fn: function (deferred) {
+			const options = {};
+			less.parse(sample, options, function (err, root, imports, options) {
+				if (err) {
+					less.writeError(err);
+					process.exit(3);
+				}
+
+				deferred.resolve();
+			});
+		}
+	})
+	.add("Chevrotain", () => parseChevrotain(sample))
+
+	.run({
+		async: false
+	});
